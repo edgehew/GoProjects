2254b369c0a336f7b0fd716297b8dc2b499c0cfb
Matthew Edge <matthewjjedge@gmail.com>
2022-06-05 09:29:55 -0700

    working hello world bot

---
+++ discord-bots/hello-world/go.mod
@@ -0,0 +1,5 @@
+module github.com/edgehew/GoProjects/discord-bots/hello-world
+
+go 1.13
+
+require github.com/bwmarrin/discordgo v0.25.0 // indirect
---
+++ discord-bots/hello-world/src/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+    //"encoding/json"
+    "flag"
+    "fmt"
+    //"io/ioutil"
+    //"net/http"
+    "os"
+    "os/signal"
+    //"strings"
+    "syscall"
+
+    "github.com/bwmarrin/discordgo"
+)
+
+// Variables used for command line parameters
+var (
+    Token string
+)
+
+func init() {
+    flag.StringVar(&Token, "t", "", "Bot Token")
+    flag.Parse()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+        return
+    }
+
+    m.Author.
+
+    if m.Content == "!hello-world" {
+        _, err := s.ChannelMessageSend(m.ChannelID, "Hello World")
+
+        if err != nil {
+            fmt.Println(err)
+        }
+    }
+}
+
+func main() {
+
+    // Create a new Discord session using the provided bot token.
+    dg, err := discordgo.New("Bot " + Token)
+    if err != nil {
+        fmt.Println("error creating Discord session,", err)
+        return
+    }
+
+    // Register the messageCreate func as a callback for MessageCreate events.
+    dg.AddHandler(messageCreate)
+
+    // In this example, we only care about receiving message events.
+    dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+    // Open a websocket connection to Discord and begin listening.
+    err = dg.Open()
+    if err != nil {
+        fmt.Println("error opening connection,", err)
+        return
+    }
+
+    // Wait here until CTRL-C or other term signal is received.
+    fmt.Println("Bot is now running. Press CTRL-C to exit.")
+    sc := make(chan os.Signal, 1)
+    signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+    <-sc
+
+    // Cleanly close down the Discord session.
+    dg.Close()
+}
